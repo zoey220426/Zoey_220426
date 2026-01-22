c323010a26b7eca605fe559677761e64d79ddd7d
zoey220426 <zoey2204261@gmail.com>
2026-01-23 12:32:44 +1300

    Update Song.js

--- Song.js
+++ Song.js
@@ -22,25 +22,23 @@ const Break = s("breaks165")
 .dec(0.105)
 .gain(0.75)
 
-const Bass = s("saw")
-.scale("c:minor")
-.oct(2)
-.n("0")
+const Bass = s("sid")
+.n("1")
 .struct(`
 [x - - -]
 [- - x -]
-[- - - -]
+[- - x -]
 [x - - -]
 `)
-.cut(0)
+.cut(1)
 .dec(0.2)
-.lpf(200)
-.lpe(2)
 .gain(0.8)
 
+
 const Bus = stack(
-Break
-).crush(12).lpf(7500).ftype("ladder")
+Break,
+Bass
+).dist(0.1).crush(12).lpf(7500).ftype("ladder")
 
 stack(
 Bus
